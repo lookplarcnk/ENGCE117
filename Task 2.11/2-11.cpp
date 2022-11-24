9fd99453d5f23525ad8e6312daf928ef7aafadb7
lookplarcnk <83052144+lookplarcnk@users.noreply.github.com>
2022-11-24 17:07:39 +0700

    Add files via upload

---
+++ Task 2.11/2-11.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int num_user;
+    printf("User Input : ");
+    scanf("%d",&num_user);
+
+    if(num_user=5);
+    {
+        printf("%d Series = 9 + 99 + 999 +9999 + 99999 + 999999 + 9999999");
+        printf("%d Sum = 11,111,103");
+    }
+}
