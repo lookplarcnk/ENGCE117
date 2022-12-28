75422e1dbef5b86c206c03f4b3842393a9a048ff
lookplarcnk <83052144+lookplarcnk@users.noreply.github.com>
2022-12-28 20:58:46 +0700

    Add files via upload

---
+++ LAB04/LAB4-3.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <string.h>
+
+struct studentNode 
+{
+    char name[20];
+    int age;
+    char sex;
+    float gpa;
+    struct studentNode *next;
+};
+
+void SaveNode(struct studentNode *child, char n[], int a, char s, float g)
+{
+    strcpy(child->name, n);
+    child->age = a;
+    child->sex = s;
+    child->gpa = g;
+}
+
+void GoNext1(struct studentNode *walk)
+{
+    if (walk != NULL)
+        walk = walk->next;
+}
+
+int main()
+{
+    struct studentNode *start = new struct studentNode;
+    SaveNode(start, "one", 6, 'M', 3.11);
+    start->next = new struct studentNode;
+    SaveNode(start->next, "two", 8, 'F', 3.22);
+    start->next->next = new struct studentNode;
+    SaveNode(start->next->next, "three", 10, 'M', 3.33);
+    start->next->next->next = new struct studentNode;
+    SaveNode(start->next->next->next, "four", 12, 'F', 3.44);
+
+    struct studentNode *now1 = start;
+    struct studentNode **now2 = &start;
+
+    GoNext1(now1);
+    printf( "%s ", now1->name );
+}
+
---
+++ LAB04/LAB4-5.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+
+struct studentNode 
+{
+    char name[20];
+    int age;
+    char sex;
+    float gpa;
+    struct studentNode *next;
+};
+
+void SaveNode (struct studentNode *child, char n[], int a, char s, float g)
+{
+    strcpy(child->name, n);
+    child->age = a;
+    child->sex = s;
+    child->gpa = g;
+}
+
+void GoNext2(struct studentNode **walk)
+{
+    if (*walk != NULL)
+    *walk = (*walk)->next;
+}
+
+int main()
+{
+    struct studentNode *start = new struct studentNode;
+    SaveNode(start, "one", 6, 'M', 3.11);
+    start->next = new struct studentNode;
+    SaveNode(start->next, "two", 8, 'F', 3.22);
+    start->next->next = new struct studentNode;
+    SaveNode(start->next, "three", 10, 'M', 3.33);
+    start->next->next = new struct studentNode;
+    SaveNode(start->next, "four", 12, 'F', 3.44);
+    start->next->next->next = new struct studentNode;
+
+    struct studentNode *now1 = start;
+    struct studentNode **now2 = &start;
+
+    GoNext2(now2);
+    printf("%s", (*now2)->name);
+    return 0;
+}
