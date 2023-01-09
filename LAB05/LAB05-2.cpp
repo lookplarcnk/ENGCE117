9f68b4f21363e2c1742a088f87d708a1c7e831c8
lookplarcnk <83052144+lookplarcnk@users.noreply.github.com>
2023-01-09 21:51:21 +0700

    Update LAB05-2.cpp

--- LAB05/LAB05-2.cpp
+++ LAB05/LAB05-2.cpp
@@ -1,74 +1,59 @@
-#include <iostream>
-#include <cstring>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 struct studentNode {
-  char name[20];
-  int age;
-  char sex;
-  float gpa;
-  struct studentNode *next;
-};
+    char name[20] ;
+    int age ;
+    char sex ;
+    float gpa ;
+    struct studentNode *next ;
+} ;
 
-struct studentNode *start;
+void InsNode(struct studentNode** now, const char* name, int age, char sex, float gpa) {
+  struct studentNode* newNode = (struct studentNode*)malloc(sizeof(struct studentNode));
+  strcpy(newNode->name, name);
+  newNode->age = age;
+  newNode->sex = sex;
+  newNode->gpa = gpa;
 
-void InsNode(struct studentNode **now, char *name, int age, char sex, float gpa) {
-  // Create a new node
-  struct studentNode *new_node = new studentNode;
-  strcpy(new_node->name, name);
-  new_node->age = age;
-  new_node->sex = sex;
-  new_node->gpa = gpa;
-  new_node->next = NULL;
-
-  // Insert the new node before the current node
-  new_node->next = *now;
-  if (*now == start) {
-    start = new_node;
-  } else {
-    struct studentNode *prev = start;
-    while (prev->next != *now) {
-      prev = prev->next;
-    }
-    prev->next = new_node;
-  }
+  newNode->next = *now;
+  *now = newNode;
 }
 
-void GoNext(struct studentNode **now){
-  if (*now != NULL) {
+void GoNext(struct studentNode** now) {
+  if(*now != NULL) {
     *now = (*now)->next;
   }
 }
 
-void DelNode(struct studentNode **now) {
-  // Check if the current node is the start of the list
-  if (*now == start) {
-    // Update the start pointer to point to the next node in the list
-    start = (*now)->next;
-  } else {
-    // Iterate through the list to find the node before the current node
-    struct studentNode;
+void DelNode(struct studentNode** now) {
+  if (*now == NULL) {
+    return;
   }
+  
+  struct studentNode* temp = *now;
+  *now = (*now)->next;
+  free(temp);
 }
 
-void ShowAll( struct studentNode **walk ){
-    while( *walk != NULL ) {
-        printf( "%s", (*walk)->name ) ;
-        walk = &(*walk)->next ;
-        }//end while
-        printf(" ");
+void ShowAll(struct studentNode** walk) {
+  while (*walk != NULL) {
+    printf("%s ", (*walk)->name);
+    walk = &(*walk)->next;
+    }//end while
+    printf(" ");
 }//end function
 
 int main() {
-    struct studentNode *start, **now ;
-    start = NULL ; now = &start ;
-    InsNode( now, "one", 6, 'M', 3.11 ) ; ShowAll( &start ) ;
-    InsNode( now, "two", 8, 'F', 3.22 ) ; ShowAll( &start ) ;
-    GoNext( &now ) ;
-    InsNode( now, "three", 10, 'M', 3.33 ) ; ShowAll( &start ) ;
-    InsNode( now, "four", 12, 'F', 3.44 ) ; ShowAll( &start ) ;
-    GoNext(&now) ;
-    DelNode(&now) ; ShowAll( &start ) ;
-    return 0 ;
+  struct studentNode *start, **now ;
+  start = NULL ; now = &start ;
+  InsNode( now, "one", 6, 'M', 3.11 ) ; ShowAll( &start ) ;
+  InsNode( now, "two", 8, 'F', 3.22 ) ; ShowAll( &start ) ;
+  GoNext( now ) ;
+  InsNode( now, "three", 10, 'M', 3.33 ) ; ShowAll( &start ) ;
+  InsNode( now, "four", 12, 'F', 3.44 ) ; ShowAll( &start ) ;
+  GoNext( now ) ;
+  DelNode( now ) ; ShowAll( &start ) ;
+  return 0 ;
 }//end function
-
-
